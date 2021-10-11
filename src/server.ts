0d463acd8708b6f2feef3614d50f5d6cda2ae89f
이호진 <kiss0104040@gmail.com>
2021-10-11 22:52:48 +0900

    #0.3 Frontend Setup

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,10 @@ import * as express from 'express'
 
 const app = express()
 
-console.log('hello')
+app.set('view engine', 'pug')
+app.set('views', __dirname + '/views')
+app.use('public', express.static(__dirname + '/public'))
+app.get('/', (req, res: express.Response) => res.render('home'))
 
-app.listen(3000)
+const handleListen = () => console.log(`Listening on http://localhost:3000`)
+app.listen(3000, handleListen)
