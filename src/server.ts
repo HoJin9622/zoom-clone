76d1aad4a62da8559a66530a53404bbe30ee936c
이호진 <kiss0104040@gmail.com>
2021-10-11 23:19:07 +0900

    #1.2 WebSockets in NodeJS

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
+import * as http from 'http'
+import * as Websocket from 'ws'
 import * as express from 'express'
 
 const app = express()
 
 app.set('view engine', 'pug')
 app.set('views', __dirname + '/views')
-app.use('public', express.static(__dirname + '/public'))
-app.get('/', (req, res: express.Response) => res.render('home'))
-app.get('/*', (req, res) => res.redirect('/'))
+app.use('/public', express.static(__dirname + '/public'))
+app.get('/', (_, res: express.Response) => res.render('home'))
+app.get('/*', (_, res: express.Response) => res.redirect('/'))
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`)
-app.listen(3000, handleListen)
+
+const server = http.createServer(app)
+const wss = new Websocket.Server({ server })
+
+server.listen(3000, handleListen)
