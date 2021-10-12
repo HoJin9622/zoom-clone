19cce3f5a510d00ae331ed406a5920a55c1a668e
이호진 <kiss0104040@gmail.com>
2021-10-12 20:42:27 +0900

    1.8 Nicknames part Two

--- src/server.ts
+++ src/server.ts
@@ -19,12 +19,22 @@ const sockets: Websocket[] = []
 
 wss.on('connection', (socket) => {
   sockets.push(socket)
+  socket['nickname'] = 'Anon'
   console.log('Connected to Brwoser ✅')
   socket.on('close', () => console.log('Disconnected from Browser ❌'))
-  socket.on('message', (message) => {
-    sockets.forEach((aSocket) => {
-      aSocket.send(message.toString())
-    })
+  socket.on('message', (msg) => {
+    const message = JSON.parse(msg.toString())
+    switch (message.type) {
+      case 'new_message':
+        sockets.forEach((aSocket) =>
+          // @ts-ignore
+          aSocket.send(`${socket['nickname']}: ${message.payload}`)
+        )
+        break
+      case 'nickname':
+        socket['nickname'] = message.payload
+        break
+    }
   })
   socket.send('Welcome!')
 })
